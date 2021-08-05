b599e764a5aee2a812b4be3399253ed60d05888e
Lippman Elsweyr <lelsweyr@si-f3.kzn.21-school.ru>
2021-08-05 14:30:11 +0300

    devide UI in home page

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -6,33 +6,44 @@ class HomePage extends StatelessWidget {
     return DefaultTabController(
       length: 4,
       child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          actions: [
-            Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Theme.of(context).accentColor,
-                ),
-                child: IconButton(
-                    icon: Icon(Icons.arrow_back_rounded),
-                    onPressed: () {},
-                    splashRadius: 25)
-                ),
-          ],
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'On hold'),
-              Tab(text: 'In progress'),
-              Tab(text: 'Needs review'),
-              Tab(text: 'Approved'),
-            ],
-          ),
-        ),
+        appBar: _appBar(context),
         body: Center(child: Text('Example')),
       ),
+    );
+  }
+
+  Widget _logOutButton(context) {
+    return Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Theme.of(context).accentColor,
+        ),
+        child: IconButton(
+            icon: Icon(Icons.arrow_back_rounded),
+            onPressed: () {},
+            splashRadius: 25));
+  }
+
+  PreferredSizeWidget _tabBar() {
+    return TabBar(
+      tabs: [
+        Tab(text: 'On hold'),
+        Tab(text: 'In progress'),
+        Tab(text: 'Needs review'),
+        Tab(text: 'Approved'),
+      ],
+    );
+  }
+
+  PreferredSizeWidget _appBar(context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      actions: [
+        _logOutButton(context),
+      ],
+      bottom: _tabBar(),
     );
   }
 }
