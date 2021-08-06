3e0d6e628f790f0f07e072ccd9bd1ec2dbeb9f7b
Lippman Elsweyr <lelsweyr@si-d5.kzn.21-school.ru>
2021-08-06 11:39:29 +0300

    Add authentication BloC, begin service part(REST API)

---
+++ lib/bloc/authentication_bloc/authentication_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_demo_app/bloc/authentication_bloc/authentication_event.dart';
+import 'package:login_demo_app/bloc/authentication_bloc/authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  AuthenticationBloc() : super(Unknown());
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+      AuthenticationEvent event) async* {
+    if (event is AppStarted) {
+      yield* _mapAppStartedToState();
+    } else if (event is LoggedIn) {
+      yield* _mapLoggedInToState();
+    } else if (event is LoggedOut) {
+      yield* _mapLoggedOutToState();
+    }
+  }
+
+  Stream<AuthenticationState> _mapAppStartedToState() async* {
+  }
+
+  Stream<AuthenticationState> _mapLoggedInToState() async* {
+  }
+
+  Stream<AuthenticationState> _mapLoggedOutToState() async* {
+  }
+}
---
+++ lib/service/login_model.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future authenticateUser(String username, String password) async {
+  ApiResponse _apiResponse = ApiResponse();
+
+  try {
+    final response = await http.post(
+        Uri.parse(
+            'https://trello.backend.tests.nekidaem.ru/api/v1/users/login/'),
+        body: {
+          'username': username,
+          'password': password,
+        });
+
+    switch (response.statusCode) {
+      case 200:
+        _apiResponse.Data = User.fromJson(json.decode(response.body));
+    }
+  } catch (e) {}
+}
+
+class ApiResponse {
+  // _data will hold any response converted into
+  // its own object. For example user.
+  Object _data = Object();
+  // _apiError will hold the error object
+  Object _apiError = Object();
+
+  Object get Data => _data;
+  set Data(Object data) => _data = data;
+
+  Object get ApiError => _apiError as Object;
+  set ApiError(Object error) => _apiError = error;
+}
+
+class User {
+  String _username = '';
+  String _password = '';
+
+  User(this._username, this._password);
+
+  String get userName => this._username;
+  set userName(String username) => this._username = username;
+
+  String get passWord => this._password;
+  set passWord(String password) => this._password = password;
+
+  User.fromJson(Map<String, dynamic> json) {
+    _username = json['username'];
+    _password = json['password'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['username'] = this._username;
+    data['password'] = this._password;
+    return data;
+  }
+}
