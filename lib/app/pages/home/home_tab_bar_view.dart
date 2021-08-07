ec514e92bdb4450fcecea0d334a77fbe8d2e280e
Lippman Elsweyr <lelsweyr@et-l6.kzn.21-school.ru>
2021-08-07 22:56:00 +0300

    Update tab_bar_view_children

--- lib/app/pages/home/home_tab_bar_view.dart
+++ lib/app/pages/home/home_tab_bar_view.dart
@@ -12,7 +12,22 @@ class HomeTabBarView extends StatelessWidget {
         });
   }
 
-  Widget? checkLoadingDataState(context, state) {
+  List<Widget> tabBarViewChildren(context, state) {
+    List<Widget> widgets = [];
+    Widget? check = _checkLoadingDataState(context, state);
+
+    for (int i = 0; i < 4; i++) {
+      if (check != null) {
+        widgets.add(check);
+      } else {
+        widgets.add(_listOfCards(context, state, i, check));
+      }
+    }
+
+    return widgets;
+  }
+
+  Widget? _checkLoadingDataState(context, state) {
     if (state is LoadingDataState) {
       return LoadingDataIndicator();
     } else if (state is LoadingDataErrorState) {
@@ -22,37 +37,24 @@ class HomeTabBarView extends StatelessWidget {
     }
   }
 
-  List<Widget> tabBarViewChildren(context, state) {
-    List<Widget> widgets = [];
-    Widget? check = checkLoadingDataState(context, state);
-
-    for (int i = 0; i < 4; i++) {
-      widgets.add(example(context, state, i, check));
-    }
-
-    return widgets;
-  }
-
-  Widget example(context, state, num, check) {
-    return (check != null)
-        ? check
-        : ListView.builder(
-            itemCount: state.cards[num]!.length,
-            itemBuilder: (context, index) {
-              var values = state.cards.values.toList();
-              return Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('ID: ${values[num][index].id}'),
-                      SizedBox(height: 5),
-                      Text('${values[num][index].text}'),
-                    ],
-                  ),
-                ),
-              );
-            });
+  Widget _listOfCards(context, state, num, check) {
+    return ListView.builder(
+        itemCount: state.cards[num]!.length,
+        itemBuilder: (context, index) {
+          var values = state.cards.values.toList();
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('ID: ${values[num][index].id}'),
+                  SizedBox(height: 5),
+                  Text('${values[num][index].text}'),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
