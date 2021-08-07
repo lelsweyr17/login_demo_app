5f5de5df77befdc95a1dcc21263357cbb01bab41
Lippman Elsweyr <lelsweyr@si-j4.kzn.21-school.ru>
2021-08-07 12:53:47 +0300

    Decode text data from string to UTF8

--- lib/app/service/api_response.dart
+++ lib/app/service/api_response.dart
@@ -1,5 +1,6 @@
-import 'dart:convert';
 import 'dart:io';
+import 'dart:convert';
+import 'dart:convert' show utf8;
 import 'package:http/http.dart' as http;
 
 String token = '';
@@ -14,15 +15,7 @@ Future<void> authenticateUser(String username, String password) async {
 
   switch (response.statusCode) {
     case 200:
-      var jsonData = json.decode(response.body)['token'];
-      token = jsonData;
-      print('jsonData: $jsonData');
-      final parts = jsonData.split(r'.');
-      print('parts: $parts');
-      assert(parts.length == 3);
-      print('OK');
-      print(jsonDecode(
-          utf8.decode(base64Url.decode(base64Url.normalize(parts[1])))));
+      token = jsonDecode(response.body)['token'];
       fetchCard();
       break;
     case 400:
@@ -33,53 +26,29 @@ Future<void> authenticateUser(String username, String password) async {
 }
 
 Future fetchCard() async {
+  List<Card> cards = [];
   var header = 'JWT ' + '$token';
-  print('JWT: $header');
+
   final response = await http.get(
     Uri.parse('https://trello.backend.tests.nekidaem.ru/api/v1/cards/'),
     headers: {HttpHeaders.authorizationHeader: header},
   );
-  print(response.body);
+
+  var jsonData = json.decode(response.body);
+
+  for (var i in jsonData) {
+    Card card = Card(row: i['row'], text: i['text']);
+    print(i['row']);
+    print(utf8.decode(i['text'].runes.toList()));
+    cards.add(card);
+  }
 }
 
-// import 'dart:convert';
-// import 'dart:io';
-// import 'package:http/http.dart' as http;
+class Card {
+  final int id;
+  final String row;
+  final int seq_num;
+  final String text;
 
-// String token = '';
-
-// Future<void> authenticateUser(String username, String password) async {
-//   final response = await http.post(
-//       Uri.parse('https://trello.backend.tests.nekidaem.ru/api/v1/users/login/'),
-//       body: {
-//         'username': username,
-//         'password': password,
-//       });
-
-//   switch (response.statusCode) {
-//     case 200:
-//       var jsonData = json.decode(ascii.decode(
-//           base64.decode(base64.normalize(response.body.split(".")[1]))));
-//       token = json.decode(response.body.split('.')[0]);
-//       print(jsonData);
-//       print(token);
-//       fetchCard();
-//       break;
-//     case 400:
-//       throw Exception('400 error');
-//     default:
-//       throw Exception('login failure');
-//   }
-// }
-
-// Future fetchCard() async {
-//   var header = 'JWT ' + token;
-//   final response = await http.get(
-//     Uri.parse('https://trello.backend.tests.nekidaem.ru/api/v1/cards/'),
-//     headers: {HttpHeaders.authorizationHeader: header},
-//   );
-//   print(token);
-//   final responseJson = jsonDecode(response.body);
-//   print(responseJson);
-// }
-
+  Card({this.id = 0, required this.row, this.seq_num = 0, required this.text});
+}
